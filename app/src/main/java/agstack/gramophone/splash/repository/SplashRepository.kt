66160efa75070fe5453c5744042824546ff2cc7a
Farhan <farhanl@gramophone.co.in>
2022-05-11 21:41:54 +0530

    language in progress

--- app/src/main/java/agstack/gramophone/splash/repository/SplashRepository.kt
+++
@@ -1,9 +0,0 @@
-package agstack.gramophone.splash.repository
-
-import agstack.gramophone.retrofit.ApiHelper
-
-
-class SplashRepository(private val apiHelper: ApiHelper) {
-
-    suspend fun getUsers() = apiHelper.getUsers()
-}
