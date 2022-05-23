23704ca4ebfcf5049731813df5e236794b727535
raj <raj@11sight.com>
2022-05-23 11:22:13 +0530

    Added api to ApiInterface

--- app/src/main/java/agstack/gramophone/retrofit/ApiService.kt
+++ app/src/main/java/agstack/gramophone/retrofit/ApiService.kt
@@ -1,13 +1,36 @@
 package agstack.gramophone.retrofit
 
+import okhttp3.ResponseBody
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface ApiService {
 
     @GET("users")
     suspend fun loginUser(): String
 
+    @POST("onboarding/onboarding-check")
+    suspend fun validateOnboarding() : String
 
+    @POST("onboarding/onboarding-check")
+    suspend fun getLangaugesList() :List<String>
+
+    @POST("onboarding/send-otp")
+    suspend fun sendMobileNo(@Body hashMap: HashMap<*, *>?): String
+
+    @POST("onboarding/resend-otp")
+     suspend fun resendMobileNo(@Body hashMap: HashMap<*, *>?): String
+
+    @POST("onboarding/update-phone-otp")
+    suspend fun sendUpdatedMobileNo(@Body hashMap: HashMap<*, *>?): Boolean
+
+    @PUT("onboarding/update-phone-no")
+    suspend fun validateUpdatePhoneOtp(@Body hashMap: HashMap<*, *>?): Boolean
+
+    @POST("onboarding/validate-otp")
+    suspend fun validateOtp(@Body hashMap: HashMap<*, *>?): ResponseBody
 
 
 }
