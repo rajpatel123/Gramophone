4f28bf3c8122060766ac4dc9b5e9ed61534c2fb6
Farhan <farhanl@gramophone.co.in>
2022-07-04 08:54:07 +0530

    checkout status

---
+++ app/src/main/java/agstack/gramophone/ui/checkout/CheckoutStatusRepository.kt
@@ -0,0 +1,22 @@
+package agstack.gramophone.ui.checkout
+
+import agstack.gramophone.di.GramoAppService
+import agstack.gramophone.ui.login.model.GenerateOtpResponseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class CheckoutStatusRepository @Inject constructor(
+    private val gramoAppService: GramoAppService
+) {
+
+    suspend fun sendOTP(loginMap: HashMap<Any, Any>): Response<GenerateOtpResponseModel> = withContext(
+        Dispatchers.IO) {
+        val popular = gramoAppService.sendOTP(loginMap)
+        popular
+    }
+
+}
