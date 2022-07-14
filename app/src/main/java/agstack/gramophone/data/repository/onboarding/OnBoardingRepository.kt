a9a8272bd01d1f744d66cdd36ab6a72c28d0f73f
raj <raj@11sight.com>
2022-07-14 15:47:05 +0530

    User Onboarding inprogress

--- app/src/main/java/agstack/gramophone/data/repository/onboarding/OnBoardingRepository.kt
+++ app/src/main/java/agstack/gramophone/data/repository/onboarding/OnBoardingRepository.kt
@@ -14,7 +14,7 @@ import javax.inject.Singleton
 interface OnBoardingRepository {
     suspend fun getLanguage(): Response<LanguageListResponse>
 
-    suspend fun sendOTP(loginMap: SendOtpRequestModel): Response<SendOtpResponseModel>
+    suspend fun sendOTP(sendOtpRequestModel: SendOtpRequestModel): Response<SendOtpResponseModel>
 
     suspend fun getInitialData(initiateAppDataRequestModel: InitiateAppDataRequestModel): Response<InitiateAppDataResponseModel>
 
