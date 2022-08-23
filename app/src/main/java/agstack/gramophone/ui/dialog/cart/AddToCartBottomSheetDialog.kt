bae8912c2f14aef115082dd63204748bca2d7030
Farhan <farhanl@gramophone.co.in>
2022-08-23 10:51:18 +0530

    Ui home

--- app/src/main/java/agstack/gramophone/ui/dialog/cart/AddToCartBottomSheetDialog.kt
+++ app/src/main/java/agstack/gramophone/ui/dialog/cart/AddToCartBottomSheetDialog.kt
@@ -1,6 +1,10 @@
 package agstack.gramophone.ui.dialog.cart
 
 import agstack.gramophone.databinding.BottomSheetAddToCartBinding
+import agstack.gramophone.ui.home.product.activity.ProductSKUAdapter
+import agstack.gramophone.ui.home.product.activity.ProductSKUOfferAdapter
+import agstack.gramophone.ui.home.view.fragments.market.model.ProductSkuListItem
+import agstack.gramophone.ui.home.view.fragments.market.model.PromotionListItem
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +13,8 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
 class AddToCartBottomSheetDialog : BottomSheetDialogFragment() {
-    lateinit var customerSupportNumber: String
+    lateinit var mSKUList: ArrayList<ProductSkuListItem?>
+    lateinit var mSkuOfferList: ArrayList<PromotionListItem?>
     var binding: BottomSheetAddToCartBinding? = null
 
     override fun onCreateView(
@@ -24,7 +29,24 @@ class AddToCartBottomSheetDialog : BottomSheetDialogFragment() {
     }
 
     private fun setupUi() {
+        /*
+         getNavigator()?.setProductSKUAdapter(
+                                ProductSKUAdapter(
+                                    mSKUList
+                                )
+                            ) {
+                                Log.d("productSKUItemSelected", it.productId.toString())
+                                selectedSkuListItem = it
+                                productDetailstoBeFetched.product_id =
+                                    selectedSkuListItem.productId!!.toInt()
 
+                                setPercentage_mrpVisibility(selectedSkuListItem, selectedOfferItem)
+
+                            }
+        * */
+
+        binding?.rvProductSku?.adapter = ProductSKUAdapter(mSKUList)
+        binding?.rvAvailableoffers?.adapter = ProductSKUOfferAdapter(mSkuOfferList)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/agstack/gramophone/ui/home/subcategory/SubCategoryNavigator.kt
+++ app/src/main/java/agstack/gramophone/ui/home/subcategory/SubCategoryNavigator.kt
@@ -3,6 +3,8 @@ package agstack.gramophone.ui.home.subcategory
 import agstack.gramophone.base.BaseNavigator
 import agstack.gramophone.ui.home.adapter.ProductListAdapter
 import agstack.gramophone.ui.home.subcategory.adapter.SubCategoryAdapter
+import agstack.gramophone.ui.home.view.fragments.market.model.ProductSkuListItem
+import agstack.gramophone.ui.home.view.fragments.market.model.PromotionListItem
 
 interface SubCategoryNavigator : BaseNavigator {
     fun setSubCategoryAdapter(
@@ -11,4 +13,9 @@ interface SubCategoryNavigator : BaseNavigator {
 
     fun setProductListAdapter(
         productListAdapter: ProductListAdapter, onAddToCartClick: ((productId: String) -> Unit))
+
+    fun openAddToCartDialog(
+        mSKUList: ArrayList<ProductSkuListItem?>,
+        mSkuOfferList: ArrayList<PromotionListItem?>
+    )
 }
