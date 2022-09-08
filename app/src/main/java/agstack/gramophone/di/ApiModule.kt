2a80d4d4782edfacd410e3a27e45defa536a10af
LENOVO <prachij@gramophone.co.in>
2022-09-09 00:10:39 +0530

    CommunityRetrofitClient

--- app/src/main/java/agstack/gramophone/di/ApiModule.kt
+++ app/src/main/java/agstack/gramophone/di/ApiModule.kt
@@ -14,6 +14,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -67,8 +68,49 @@ object ApiModule {
             chain.proceed(request)
         }
     }
+    private fun getCommunityHeaderInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            val request =
+                chain.request().newBuilder()
+                    .header(
+                        Constants.AUTHORIZATION,
+                        "Bearer " + SharedPreferencesHelper.instance?.getString(
+                            SharedPreferencesKeys.session_token
+                        )!!
+                    )
+                    .build()
+            chain.proceed(request)
+        }
+    }
 
+    @Provides
+    @Singleton
+    @Named("community")
+    fun provideCommunityOkHttpClient(logging: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(getCommunityHeaderInterceptor())
+            .addInterceptor(logging)
+            .connectTimeout(15, TimeUnit.SECONDS) // connect timeout
+            .readTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
 
+    @Provides
+    @Singleton
+    @Named("community")
+    fun provideCommunityRetrofit(@Named("community") client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL_SOCIAL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCommunityService(@Named("community") retrofit: Retrofit): CommunityAppService {
+        return retrofit.create(CommunityAppService::class.java)
+    }
 
 
 }
