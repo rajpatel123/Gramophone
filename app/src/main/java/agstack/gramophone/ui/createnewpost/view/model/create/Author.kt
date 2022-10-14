4346f744af0ed9c4119c36943435c8f2b2e5156a
raj <raj@11sight.com>
2022-10-14 11:11:57 +0530

    commented crete community

---
+++ app/src/main/java/agstack/gramophone/ui/createnewpost/view/model/create/Author.kt
@@ -0,0 +1,9 @@
+package agstack.gramophone.ui.createnewpost.view.model.create
+
+data class Author(
+    val _id: String,
+    val badge: Any,
+    val photoUrl: String,
+    val username: String,
+    val uuid: String
+)
