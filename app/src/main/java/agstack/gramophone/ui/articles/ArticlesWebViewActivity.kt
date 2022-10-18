20b52746e06bcd557e20708d6ea7d9b624e18881
Farhan <farhanl@gramophone.co.in>
2022-10-18 17:51:47 +0530

    article enhancement

--- app/src/main/java/agstack/gramophone/ui/articles/ArticlesWebViewActivity.kt
+++ app/src/main/java/agstack/gramophone/ui/articles/ArticlesWebViewActivity.kt
@@ -2,6 +2,7 @@ package agstack.gramophone.ui.articles
 
 import agstack.gramophone.BuildConfig
 import agstack.gramophone.R
+import agstack.gramophone.utils.Constants
 import android.app.Activity
 import android.graphics.Bitmap
 import android.os.Bundle
@@ -9,6 +10,7 @@ import android.view.View
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.amnix.xtension.extensions.isNotNullOrEmpty
 import kotlinx.android.synthetic.main.activity_articles.*
 
 class ArticlesWebViewActivity : Activity() {
@@ -21,35 +23,47 @@ class ArticlesWebViewActivity : Activity() {
 
     private fun setUp() {
         val webSettings: WebSettings = webView.settings
-        webSettings.setJavaScriptEnabled(true)
-        webSettings.domStorageEnabled = true
-        webSettings.loadWithOverviewMode = true
-        webSettings.useWideViewPort = true
-        webSettings.builtInZoomControls = true
-        webSettings.displayZoomControls = false
-        webSettings.setSupportZoom(true)
-        webSettings.defaultTextEncodingName = "utf-8"
-        webSettings.loadWithOverviewMode = true
-        webSettings.setSupportMultipleWindows(true)
-        webSettings.javaScriptCanOpenWindowsAutomatically = false
-        webView.webViewClient = object : WebViewClient() {
+        val bundle = intent.extras
+        if (bundle?.containsKey(Constants.PAGE_URL)!! && bundle.getString(Constants.PAGE_URL) != null) {
+            val pageURL = bundle.getString(Constants.PAGE_URL, "")
+            webSettings.javaScriptEnabled = true
+            webSettings.domStorageEnabled = true
+            webSettings.loadWithOverviewMode = true
+            webSettings.useWideViewPort = true
+            webSettings.builtInZoomControls = true
+            webSettings.displayZoomControls = false
+            webSettings.setSupportZoom(false)
+            webSettings.defaultTextEncodingName = "utf-8"
+            webSettings.loadWithOverviewMode = true
+            webSettings.setSupportMultipleWindows(true)
+            webSettings.javaScriptCanOpenWindowsAutomatically = false
+            webView.webViewClient = object : WebViewClient() {
 
-            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-                super.onPageStarted(view, url, favicon)
-                progressBar.visibility = View.VISIBLE
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    super.onPageStarted(view, url, favicon)
+                    progressBar.visibility = View.VISIBLE
+                }
+
+                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                    if (url.equals(pageURL + "articles")) {
+                        rlBack.visibility = View.VISIBLE
+                    } else {
+                        rlBack.visibility = View.GONE
+                    }
+                    return false
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    progressBar.visibility = View.GONE
+                }
             }
 
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                return false
-            }
-
-            override fun onPageFinished(view: WebView?, url: String?) {
-                super.onPageFinished(view, url)
-                progressBar.visibility = View.GONE
-            }
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
+            if (pageURL.isNotNullOrEmpty())
+                webView.loadUrl(pageURL)
         }
 
-        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
-        webView.loadUrl(BuildConfig.BASE_URL_ARTICLES)
+        rlBack.setOnClickListener { finish() }
     }
 }
