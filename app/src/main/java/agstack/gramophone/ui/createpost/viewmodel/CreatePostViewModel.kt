3216c248c3a74a767452f2678366ca5a68279e97
raj <raj@11sight.com>
2022-10-19 16:42:20 +0530

    Home Community design done

--- app/src/main/java/agstack/gramophone/ui/createpost/viewmodel/CreatePostViewModel.kt
+++ app/src/main/java/agstack/gramophone/ui/createpost/viewmodel/CreatePostViewModel.kt
@@ -6,8 +6,11 @@ import agstack.gramophone.data.repository.community.CommunityRepository
 import agstack.gramophone.data.repository.onboarding.OnBoardingRepository
 import agstack.gramophone.data.repository.product.ProductRepository
 import agstack.gramophone.ui.createnewpost.model.MentionRequestModel
+import agstack.gramophone.ui.createnewpost.view.model.create.CreatePostResponseModel
 import agstack.gramophone.ui.createpost.CreatePostNavigator
+import agstack.gramophone.ui.home.view.fragments.market.model.CropData
 import agstack.gramophone.ui.home.view.fragments.market.model.CropResponse
+import agstack.gramophone.ui.postdetails.model.Image
 import agstack.gramophone.ui.postdetails.view.PostDetailsActivity
 import agstack.gramophone.ui.tagandmention.Tag
 import agstack.gramophone.utils.Constants
@@ -15,6 +18,7 @@ import agstack.gramophone.utils.Constants.POST_ID
 import agstack.gramophone.utils.FileUploadRequestBody
 import agstack.gramophone.utils.Utility
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.databinding.ObservableField
 import androidx.lifecycle.viewModelScope
@@ -35,11 +39,15 @@ class CreatePostViewModel @Inject constructor(
     private val onBoardingRepository: OnBoardingRepository,
     private val productRepository: ProductRepository
 ) : BaseViewModel<CreatePostNavigator>() {
+    private lateinit var postId: String
+    private lateinit var response: retrofit2.Response<CreatePostResponseModel>
+    lateinit var imageUrls: List<Image>
     lateinit var cropResponse: CropResponse
-    var imageNo: Int =Constants.IV_ONE
+    var imageNo: Int = Constants.IV_ONE
     var listOfImages: HashMap<String, File> = hashMapOf()
     var description = ObservableField<String>()
     var tags = ArrayList<JSONObject>()
+    var removedImages = ArrayList<JSONObject>()
     fun finishActivity() {
         getNavigator()?.finishActivity()
     }
@@ -73,18 +81,36 @@ class CreatePostViewModel @Inject constructor(
         when (v.id) {
             R.id.ivDeletBig -> {
                 imageNo = Constants.IV_ONE
-                listOfImages.remove("1")
+                if (listOfImages.containsKey("1")) {
+                    listOfImages.remove("1")
+                } else {
+                    removedImages.add(JSONObject().apply {
+                        put("id", imageUrls[0].id)
+                    })
+                }
             }
 
             R.id.ivDeleteSmall1 -> {
                 imageNo = Constants.IV_TWO
-                listOfImages.remove("2")
-
+                if (listOfImages.containsKey("2")) {
+                    listOfImages.remove("2")
+                } else {
+                    removedImages.add(JSONObject().apply {
+                        put("id", imageUrls[1].id)
+                    })
+                }
             }
 
             R.id.ivDeleteSmall2 -> {
                 imageNo = Constants.IV_THREE
-                listOfImages.remove("3")
+                if (listOfImages.containsKey("3")) {
+                    listOfImages.remove("3")
+                } else {
+                    removedImages.add(JSONObject().apply {
+                        put("id", imageUrls[2].id)
+                    })
+                }
+
             }
         }
         getNavigator()?.updateAddDeleteBtn(imageNo)
@@ -104,61 +130,126 @@ class CreatePostViewModel @Inject constructor(
                     if (!listOfImages.isEmpty()){
                       val keys = listOfImages.keys
                        if (keys.size>0){
-                           if (keys.size==1){
                                keys.forEach {
-                                   val imageUpoadRequestBody = FileUploadRequestBody(listOfImages.get(it)!!)
-                                   image1 = MultipartBody.Part.createFormData(
-                                       "image",
-                                       listOfImages.get(it)!!.name,
-                                       imageUpoadRequestBody
-                                   )
-
-                               }
-
-                           }else if (keys.size==2){
-                               keys.forEach {
-                                   when(it){
-                                       "1"->{
-                                           val imageUpoadRequestBody = FileUploadRequestBody(listOfImages.get(it)!!)
+                                   when (it) {
+                                       "1" -> {
+                                           val imageUpoadRequestBody = FileUploadRequestBody(
+                                               listOfImages[it]!!
+                                           )
+                                           Log.d("Image", "" + listOfImages[it]?.path)
                                            image1 = MultipartBody.Part.createFormData(
                                                "image",
                                                listOfImages.get(it)!!.name,
                                                imageUpoadRequestBody
                                            )
                                        }
-                                       "2"->{
-                                           val imageUpoadRequestBody = FileUploadRequestBody(listOfImages.get(it)!!)
+                                       "2" -> {
+                                           val imageUpoadRequestBody = FileUploadRequestBody(
+                                               listOfImages[it]!!
+                                           )
+                                           Log.d("Image", "" + listOfImages[it]?.path)
+
                                            image2 = MultipartBody.Part.createFormData(
                                                "image",
                                                listOfImages.get(it)!!.name,
                                                imageUpoadRequestBody
                                            )
                                        }
+                                       "3" -> {
+                                           val imageUpoadRequestBody = FileUploadRequestBody(
+                                               listOfImages[it]!!
+                                           )
+                                           Log.d("Image", "" + listOfImages[it]?.path)
 
+                                           image3 = MultipartBody.Part.createFormData(
+                                               "image",
+                                               listOfImages.get(it)!!.name,
+                                               imageUpoadRequestBody
+                                           )
+                                       }
                                    }
 
                                }
-                           }else if (keys.size==3){
+
+
+                       }
+
+
+                    }
+                    response = communityRepository.createPost(desc, tags, image1, image2, image3)
+                    if (response.isSuccessful) {
+                        getNavigator()?.openAndFinishActivity(PostDetailsActivity::class.java,
+                            Bundle().apply {
+                                putString(POST_ID, response.body()?.data?._id)
+                            }
+                        )
+                    } else {
+                        getNavigator()?.showToast(Utility.getErrorMessage(response.errorBody()))
+                    }
+
+                } else
+                    getNavigator()?.onError(getNavigator()?.getMessage(R.string.no_internet)!!)
+            } catch (ex: Exception) {
+
+                when (ex) {
+                    is IOException -> getNavigator()?.onError(getNavigator()?.getMessage(R.string.network_failure)!!)
+                    else -> getNavigator()?.onError(getNavigator()?.getMessage(R.string.some_thing_went_wrong)!!)
+                }
+            }
+
+        }
+
+
+    }
+
+
+    fun updatePost() {
+        viewModelScope.launch {
+            try {
+                if (getNavigator()?.isNetworkAvailable() == true) {
+                    val desc: RequestBody =
+                        description.get()?.toRequestBody("text/plain".toMediaTypeOrNull())
+                            ?: "".toRequestBody("text/plain".toMediaTypeOrNull())
+                    val tags: RequestBody =
+                        tags.toString().toRequestBody("text/plain".toMediaTypeOrNull())
+
+                    var image1: MultipartBody.Part? = null
+                    var image2: MultipartBody.Part? = null
+                    var image3: MultipartBody.Part? = null
+                    if (!listOfImages.isEmpty()) {
+                        val keys = listOfImages.keys
+                        if (keys.size > 0) {
                             keys.forEach {
-                                when(it){
-                                    "1"->{
-                                        val imageUpoadRequestBody = FileUploadRequestBody(listOfImages.get(it)!!)
+                                when (it) {
+                                    "1" -> {
+                                        val imageUpoadRequestBody = FileUploadRequestBody(
+                                            listOfImages[it]!!
+                                        )
+                                        Log.d("Image", "" + listOfImages[it]?.path)
                                         image1 = MultipartBody.Part.createFormData(
                                             "image",
                                             listOfImages.get(it)!!.name,
                                             imageUpoadRequestBody
                                         )
                                     }
-                                    "2"->{
-                                        val imageUpoadRequestBody = FileUploadRequestBody(listOfImages.get(it)!!)
+                                    "2" -> {
+                                        val imageUpoadRequestBody = FileUploadRequestBody(
+                                            listOfImages[it]!!
+                                        )
+                                        Log.d("Image", "" + listOfImages[it]?.path)
+
                                         image2 = MultipartBody.Part.createFormData(
                                             "image",
                                             listOfImages.get(it)!!.name,
                                             imageUpoadRequestBody
                                         )
                                     }
-                                    "3"->{
-                                        val imageUpoadRequestBody = FileUploadRequestBody(listOfImages.get(it)!!)
+                                    "3" -> {
+                                        val imageUpoadRequestBody = FileUploadRequestBody(
+                                            listOfImages[it]!!
+                                        )
+                                        Log.d("Image", "" + listOfImages[it]?.path)
+
                                         image3 = MultipartBody.Part.createFormData(
                                             "image",
                                             listOfImages.get(it)!!.name,
@@ -166,20 +257,38 @@ class CreatePostViewModel @Inject constructor(
                                         )
                                     }
                                 }
+
                             }
-                           }
-                       }
+
+
+                        }
 
 
                     }
-                    val response = communityRepository.createPost(desc,tags,image1,image2,image3)
+
+                    val removeImage: RequestBody =
+                        removedImages.toString().toRequestBody("text/plain".toMediaTypeOrNull())
+
+                    val postID: RequestBody =
+                        postId.toString().toRequestBody("text/plain".toMediaTypeOrNull())
+                    response = communityRepository.updatePost(
+                        desc,
+                        tags,
+                        removeImage,
+                        postID,
+                        image1,
+                        image2,
+                        image3
+                    )
+
                     if (response.isSuccessful) {
-                        getNavigator()?.openAndFinishActivity(PostDetailsActivity::class.java,
+                        getNavigator()?.openAndFinishActivity(
+                            PostDetailsActivity::class.java,
                             Bundle().apply {
-                                putString(POST_ID,response.body()?.data?._id)
+                                putString(POST_ID, response.body()?.data?._id)
                             }
                         )
-                    }else{
+                    } else {
                         getNavigator()?.showToast(Utility.getErrorMessage(response.errorBody()))
                     }
 
@@ -204,8 +313,8 @@ class CreatePostViewModel @Inject constructor(
                 val response = onBoardingRepository.getHasTags(MentionRequestModel(text))
                 if (response.isSuccessful) {
                     val a = response.body()?.gp_api_response_data
-                    val tags =  arrayListOf<Tag>().apply {
-                        for(n in 0..(a?.size!!)-1){
+                    val tags = arrayListOf<Tag>().apply {
+                        for (n in 0..(a?.size!!) - 1) {
                             add(Tag("", null, a?.get(n)?.tag, "", "", ""))
                         }
                     }
@@ -240,12 +349,38 @@ class CreatePostViewModel @Inject constructor(
 
 
     fun getPostDetails(postId: String) {
+        this.postId = postId
         viewModelScope.launch {
             getNavigator()?.onLoading()
             try {
                 if (getNavigator()?.isNetworkAvailable() == true) {
                     val response = communityRepository.getPostDetails(postId)
                     if (response.isSuccessful) {
+                        val data = response.body()?.data
+                        description.set(data?.description.toString())
+
+                        if (data?.images != null && data.images.size > 0) {
+                            getNavigator()?.loadImages(data.images)
+                        }
+                        if (data?.tags != null && data?.tags.size > 0) {
+                            getNavigator()?.showTags(arrayListOf<CropData>().apply {
+                                for (n in 0..(data?.tags?.size!!) - 1) {
+                                    add(CropData(null, data?.tags[n].tag, null, false, "", ""))
+                                }
+
+                            })
+                            data?.tags.forEach { tag ->
+                                if (cropResponse.gpApiResponseData?.cropsList?.size!! > 0) {
+                                    cropResponse.gpApiResponseData?.cropsList?.forEach { cropData ->
+                                        if (tag.tag.equals(cropData.cropName)) {
+                                            cropData.isSelected = true
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+
 
                     } else {
                         getNavigator()?.onError(Utility.getErrorMessage(response.errorBody()))
@@ -268,6 +403,7 @@ class CreatePostViewModel @Inject constructor(
 
                 val response = productRepository.getCrops()
                 if (response.isSuccessful) {
+                    getNavigator()?.getPostDetails()
                    this@CreatePostViewModel.cropResponse = response.body()!!
                 } else {
                     getNavigator()?.showToast(Utility.getErrorMessage(response.errorBody()))
