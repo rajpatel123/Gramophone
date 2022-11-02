18bb365ab6beee1f00aa9d74cf6e490fe098d545
raj <raj@11sight.com>
2022-11-02 14:26:30 +0530

    Bug Fixes

--- app/src/main/java/agstack/gramophone/ui/favourite/model/Data.kt
+++ app/src/main/java/agstack/gramophone/ui/favourite/model/Data.kt
@@ -2,7 +2,7 @@ package agstack.gramophone.ui.favourite.model
 
 data class Data(
     val currency: String,
-    val mrp_price: Any,
+    val mrp_price: Float,
     val product_app_name: String,
     val product_id: Int,
     val product_image: String,
