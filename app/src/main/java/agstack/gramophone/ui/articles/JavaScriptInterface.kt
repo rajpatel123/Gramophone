742c2e311011870e6b92ba3baea1e295274ace7e
Farhan <farhanl@gramophone.co.in>
2022-11-04 07:02:14 +0530

    sub text added and JS event handling done

--- app/src/main/java/agstack/gramophone/ui/articles/JavaScriptInterface.kt
+++ app/src/main/java/agstack/gramophone/ui/articles/JavaScriptInterface.kt
@@ -1,15 +1,5 @@
 package agstack.gramophone.ui.articles
 
-import android.content.Context
-import android.webkit.JavascriptInterface
-import android.widget.Toast
-
-class JavaScriptInterface internal constructor(c: Context) {
-    var mContext: Context = c
-
-    @JavascriptInterface
-    fun getProductId(productId: Int?) {
-        Toast.makeText(mContext, productId.toString(), Toast.LENGTH_SHORT).show()
-    }
-
+abstract class JavaScriptInterface {
+    abstract fun goToProduct(productId: String)
 }
