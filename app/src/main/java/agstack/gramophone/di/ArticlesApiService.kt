8a185de017900f996d129dd6f64ee37e61921938
Farhan <farhanl@gramophone.co.in>
2022-11-12 19:11:18 +0530

    production url change

--- app/src/main/java/agstack/gramophone/di/ArticlesApiService.kt
+++ app/src/main/java/agstack/gramophone/di/ArticlesApiService.kt
@@ -12,7 +12,8 @@ interface ArticlesApiService {
     @GET("wp-json/wp/v2/posts?sticky=1&orderby=featured_post_sorting&_embed&per_page=5")
     suspend fun getFeaturedArticles(): Response<FeaturedArticlesResponse>
 
-    @GET("wp-json/gp/v1/popular-posts?embed&limit=5")
+    /*@GET("wp-json/gp/v1/popular-posts?embed&limit=5")*/
+    @GET("wp-json/gp/v1/popular-posts?embed")
     suspend fun getTrendingArticles(): Response<TrendingArticlesResponse>
 
     @GET("wp-json/gp/v1/posts-by-field?post_type=post&limit=5&suggested_crops=Wheat,Potato,Sugarcane")
