a0e105ed4be785513e0b69763cbbfb5adb789354
raj <raj@11sight.com>
2022-11-15 10:26:13 +0530

    Bug Fixes

---
+++ app/src/main/java/agstack/gramophone/ui/advisory/AdvisoryActivity.kt
@@ -0,0 +1,26 @@
+package agstack.gramophone.ui.advisory
+
+import agstack.gramophone.BR
+import agstack.gramophone.R
+import agstack.gramophone.base.BaseActivityWrapper
+import agstack.gramophone.databinding.ActivityAdvisoryBinding
+import agstack.gramophone.ui.home.subcategory.SubCategoryNavigator
+import agstack.gramophone.ui.home.subcategory.SubCategoryViewModel
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+
+class AdvisoryActivity : BaseActivityWrapper<ActivityAdvisoryBinding,SubCategoryNavigator,SubCategoryViewModel>() {
+
+private val subCategoryViewModel : SubCategoryViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun getLayoutID(): Int = R.layout.activity_advisory
+
+    override fun getBindingVariable(): Int = BR.viewModel
+
+    override fun getViewModel(): SubCategoryViewModel = subCategoryViewModel
+}
